27e8eb80c5ae333e73b2889c271d5278dfab176c
ZenWarrior_HT3 <152799688+ZenWarriorHT3@users.noreply.github.com>
2026-06-12 22:03:42 +0530

    morse_encoder.py

---
+++ morse_encoder.py
@@ -0,0 +1,55 @@
+text = input("ENTER THE CODE: ")
+
+morse_code = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+}
+
+
+def text_to_morse(text):
+    text = text.upper()
+    morse = ""
+    for character in text:
+        if character == " ":
+            morse += "/ "
+        elif character in morse_code:
+            morse += morse_code[character] + " "
+    return morse
+
+
+morse = text_to_morse(text)
+print("MORSE CODE: ", morse)
